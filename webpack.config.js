075aea1023ccb7d9085709459491a2d71bc947a8
Wendy Dherin <wendydherin@gmail.com>
2018-08-18 20:55:35 +0100

    Add css loaders and babelrc file. Add dist to gitignore.

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,12 @@ module.exports = {
     rules: [
       {
         use: 'babel-loader',
-        test: /\.js$/
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/,
       }
     ]
   }
